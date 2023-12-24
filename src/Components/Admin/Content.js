e4fddcca3778ed4357ab6fddcd11b020348a7f71
navi4347 <naveenkonda.dev@gmail.com>
2023-12-24 09:10:53 +0530

    Test1

--- src/Components/Admin/Content.js
+++ src/Components/Admin/Content.js
@@ -1,36 +1,42 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { VectorMap } from '@south-paw/react-vector-maps';
-import world from './Assets/world.json'; // Make sure the path is correct
+import world from './Assets/world.json';
 
 const Content = () => {
-  // Assuming 'world' variable holds the map data
   const mapData = {
     ...world,
-    // Additional map configuration if needed
+    style: {
+      fill: 'red',
+      stroke: 'red',
+    },
+  };
+   const mapContainerStyle = {
+    outline: 'none', 
   };
 
   return (
     <Container>
       <Row>
-        <Col lg={4}>
-          1 of 3
+        <Col lg={3}>
+          1 of 4
         </Col>
-        <Col lg={4}>
-          Variable width content
+        <Col lg={3}>
+          2 of 4
         </Col>
-        <Col lg={4}>
-          3 of 3
+        <Col lg={3}>
+          3 of 4
+        </Col>
+        <Col lg={3}>
+          4 of 4
         </Col>
       </Row>
       <Row>
         <Col lg={6}>
           1 of 3
         </Col>
-        <Col lg={6}>
+        <Col lg={6} style={mapContainerStyle}>
           <VectorMap {...mapData} />
         </Col>
       </Row>
