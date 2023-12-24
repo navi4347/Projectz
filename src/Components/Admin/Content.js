87dd247cea5e3bc2ddbe0bb075a7a091589ec209
navi4347 <naveenkonda.dev@gmail.com>
2023-12-24 22:30:16 +0530

    Test1

--- src/Components/Admin/Content.js
+++ src/Components/Admin/Content.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { VectorMap } from '@south-paw/react-vector-maps';
 import world from './Assets/world.json';
+import { Card } from 'react-bootstrap';
 
 const Content = () => {
   const mapData = {
@@ -12,36 +13,49 @@ const Content = () => {
       stroke: 'red',
     },
   };
-   const mapContainerStyle = {
-    outline: 'none', 
+
+  const mapContainerStyle = {
+    outline: 'none',
   };
 
+  // Array of background-color values for each card
+  const cardColors = [
+    'linear-gradient(45deg,#4099ff,#73b4ff)',
+    'linear-gradient(45deg,#2ed8b6,#59e0c5)',
+    'linear-gradient(45deg, rgba(129, 140, 248, 1) 0%, rgba(129, 140, 248, 0) 100%)',
+    'linear-gradient(45deg,#FF5370,#ff869a)',
+  ];
+
+  // Array of information for each card
+  const cardInfo = ['Seller Info', 'Vouchers', 'Clients', 'Employees'];
+
   return (
-    <Container>
-      <Row>
-        <Col lg={3}>
-          1 of 4
-        </Col>
-        <Col lg={3}>
-          2 of 4
-        </Col>
-        <Col lg={3}>
-          3 of 4
-        </Col>
-        <Col lg={3}>
-          4 of 4
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={6}>
-          1 of 3
-        </Col>
-        <Col lg={6} style={mapContainerStyle}>
-          <VectorMap {...mapData} />
-        </Col>
-      </Row>
-    </Container>
+    <div className="mt-4">
+      <Container>
+        <Row>
+          {[0, 1, 2, 3].map((index) => (
+            <Col lg={3} key={index}>
+              <Card style={{ background: cardColors[index], color: '#ffffff' }}>
+                <Card.Body>
+                  <p><strong>Important:</strong> {cardInfo[index]}</p>
+                  <p>Example of additional information in a card body.</p>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+
+        <div className="mt-4">
+          <Row>
+            <Col lg={6}>1 of 3</Col>
+            <Col lg={6} style={mapContainerStyle}>
+              <VectorMap {...mapData} />
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </div>
   );
-}
+};
 
 export default Content;
